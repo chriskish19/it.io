12133f1843a9692e8d86b24b2f36fba74667013b
chriskish19 <68289629+chriskish19@users.noreply.github.com>
2025-03-27 18:00:32 -0600

    looking ok

---
+++ scripts/about.js
@@ -0,0 +1,16 @@
+import {text} from "../classes/text.js"
+
+const typing_title = new text("0","../images/cursor.svg",30,30,10000);
+typing_title.type(100);
+
+const typing_title1 = new text("1","../images/cursor.svg",30,30,10000);
+typing_title1.type(100);
+
+const typing_title2 = new text("2","../images/cursor.svg",30,30,10000);
+typing_title2.type(100);
+
+const typing_title3 = new text("3","../images/cursor.svg",30,30,10000);
+typing_title3.type(100);
+
+const typing_title4 = new text("4","../images/cursor.svg",30,30,10000);
+typing_title4.type(100);
